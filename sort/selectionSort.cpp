590b56e3a56b433ac6075e67b1a53d2be10da026
07-khanh <khanh.npn07@gmail.com>
2026-04-07 08:04:37 +0700

    Enhance selectionSort.cpp with comments
    Added comments explaining loop invariant and time complexity for selection sort.

--- sort/selectionSort.cpp
+++ sort/selectionSort.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+/* 
+Loop invariant: At the start of each interation of the outer FOR loop, A[0..j-1] is sorted and consists of j smallest elements.
+After the first n-1 iterations, A[0..n-2] consists of n-1 smallest elements -> A[n-1] is automatically the largest.
+
+Time Complexity: Theta(n^2) for all cases.
+*/
+
 void SELECTION_SORT(int a[], int n) {
     for (int i{0}; i < n-1; ++i) {        
         int mini{i};
